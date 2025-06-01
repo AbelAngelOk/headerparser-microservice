446ad32555b912013a546954cef4dc7deba1a853
abel.cejas <abel.cejas@crowdaronline.com>
2025-05-31 21:15:01 -0300

    vercel.json

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -20,9 +19,18 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/whoami', function (req, res) {
+  const ipClient = req.ip;
+  const languageClient = req.headers['accept-language'];
+  const softwareClient = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ipClient,
+    languageClient: languageClient,
+    software: softwareClient
+  });
+  
+  })
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
