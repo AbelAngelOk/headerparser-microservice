6ecdf8109c5b4fcddf52f53df4c88c065f9287b6
abel.cejas <abel.cejas@crowdaronline.com>
2025-05-31 21:27:07 -0300

    restore app.get original

--- index.js
+++ index.js
@@ -14,15 +14,10 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-/*
-  app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-  }); 
-*/	
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+}); 
 
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
